fd36384e32ab392ecbaf0612612140a8a4d609aa
Stephord06 <wongjun808@gmail.com>
2026-04-13 08:45:19 +0800

    Create new Class and Design it -- AppointmentDetails

---
+++ JAVAGROUPWORK/src/technician/AppointmentDetails.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package technician;
+
+/**
+ *
+ * @author wongj
+ */
+import javax.swing.*;
+import java.awt.*;
+public class AppointmentDetails {
+    // UI Interface Design:
+    JFrame 
+}
