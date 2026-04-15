12c5974e097fd5c5b842957cf7f42c659fd3f182
Stephord06 <wongjun808@gmail.com>
2026-04-15 08:41:38 +0800

    Update coding for TechnicianDashBoard

--- JAVAGROUPWORK/src/technician/AppointmentDetails.java
+++ JAVAGROUPWORK/src/technician/AppointmentDetails.java
@@ -10,7 +10,34 @@ package technician;
  */
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
+
 public class AppointmentDetails {
-    // UI Interface Design:
-    JFrame 
+    
+    public void AppointDetails(){
+        // UI Interface Design:
+        
+        // JFrame Settings
+        JFrame appointDetails = new JFrame("Appointment Details");
+        appointDetails.setSize(1920,1080);
+        appointDetails.setLayout(null);
+        
+        // Jlabel Settings
+        JLabel label1 = new JLabel("Appointment ID:");
+        
+        
+        // JButton Settings
+        
+        
+        // JPanel Settings
+        
+        
+        // Display objects
+        appointDetails.setVisible(true);
+    } 
+            
+    
+    public static void main(String [] args){
+        new AppointmentDetails().AppointDetails();
+    }
 }
